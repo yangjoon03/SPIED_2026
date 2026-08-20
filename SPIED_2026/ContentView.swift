ecb5714809470fbaed565c5f48d4c61816f82838
YangjunYeong <pythoncloud0115@gmail.com>
2026-08-20 23:02:18 +0900

    Initial Commit

---
+++ SPIED_2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPIED_2026
+//
+//  Created by yjy on 8/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SPIED_2026/SPIED_2026App.swift
@@ -0,0 +1,17 @@
+//
+//  SPIED_2026App.swift
+//  SPIED_2026
+//
+//  Created by yjy on 8/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SPIED_2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
